1d8b3f556d002abbde6a30d2f11b59990a5e78eb
Asten Fuller <astenhfuller@gmail.com>
2019-12-13 12:29:43 -0800

    Ability to add planets + fun with css

--- main.js
+++ main.js
@@ -16,8 +16,8 @@ var planets = [
 planets.forEach(populateDropdown);
 
 function populateDropdown(item){
-    var option = document.createElement("option");
-    var planetSelected = option.textContext = item[0];
+    let option = document.createElement("option");
+    let planetSelected = option.textContext = item[0];
     option.value = item[1];
     document.getElementById("planets").appendChild(option).innerHTML = planetSelected;
     option.setAttribute("id", item[0]);
@@ -26,25 +26,33 @@ function populateDropdown(item){
 
 
 function calculateWeight(weight, planetName) {
-    var result = weight * planetName;
+    let result = weight * planetName;
     return result;
 }
 
 function handleClickEvent() {
-    
-    var userWeight = document.getElementById("user-weight").value;
-    var select = document.getElementById("planets");
-    var planetName = select.options[select.selectedIndex].text;
-    var planetSize = document.getElementById("planets").value;
-    var result = calculateWeight(userWeight, planetSize);
+    let userWeight = document.getElementById("user-weight").value;
+
+    if (isNaN(userWeight)){
+        document.getElementById("output").innerHTML = "That is not a number";
+    }else if (userWeight < 0){
+        document.getElementById("output").innerHTML = "You chose a negative weight";
+    }else{
+    let select = document.getElementById("planets");
+    let planetName = select.options[select.selectedIndex].text;
+    let planetSize = document.getElementById("planets").value;
+    let result = calculateWeight(userWeight, planetSize);
 
     document.getElementById("output").innerHTML = "If you were on " + planetName + ", you would weigh " + result + "lbs!";
+    }
 }
 
     function handleCheckBox() {
         if (planets[0].includes("Pluto")){
             document.getElementById("Pluto").remove();
             planets[0][0] = "";
+            document.getElementById("output").innerHTML = "Pluto is not a planet!";
+
             
         }else{
             planets[0][0] = "Pluto";
@@ -55,8 +63,32 @@ function handleClickEvent() {
             let pluto = document.getElementById("planets");
             pluto.prepend(option);
             pluto.selectedIndex = "0";
+            document.getElementById("output").innerHTML = "Pluto is a planet!";
+
            
             
         }
 
 }
+
+    function handleAddPlanet() {
+        let planetName = document.getElementById("new-planet").value;
+        let planetValue = document.getElementById("new-planet-value").value;
+        let planetOption = document.getElementById("planets");
+
+        planets.push([planetName, planetValue]);
+
+        if (isNaN(planetValue)){
+            document.getElementById("new-output").innerHTML = "Your value is not a number";
+        }else {
+            let option = document.createElement("option");
+            option.setAttribute("id", planetName);
+            option.setAttribute("value", planetValue);
+            option.innerHTML = planetName;
+            planetOption.appendChild(option);
+
+            document.getElementById("new-output").innerHTML = `${planetName} has been added`;
+            document.getElementById("new-planet").value = "";
+            document.getElementById("new-planet-value").value = "";
+        }
+    }
